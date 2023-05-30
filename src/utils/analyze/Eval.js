743d79b0435d8395d2f5fb337b427545eac0ec45
Felix Steinbrecht <steinbrecht@proton.me>
2023-05-30 03:17:46 +0200

    Remove top level async

--- src/utils/analyze/Eval.js
+++ src/utils/analyze/Eval.js
@@ -1,16 +1,23 @@
-const stockfish = await Stockfish()
-stockfish.postMessage('setoption name use nnue value true')
-stockfish.addMessageListener((message) => {
-  if (message.startsWith('info depth')) {
-    const scoreMatch = message.match(/score cp (-?\d+)/)
-    if (scoreMatch) {
-      const score = parseInt(scoreMatch[1])
-      console.log(score)
-    }
-  }
-})
+const stockfish = undefined
 
-export function evaluate(fen, depth) {
+async function startStockfish() {
+  if (stockfish === undefined) {
+    stockfish = await Stockfish()
+    stockfish.postMessage('setoption name use nnue value true')
+    stockfish.addMessageListener((message) => {
+      if (message.startsWith('info depth')) {
+        const scoreMatch = message.match(/score cp (-?\d+)/)
+        if (scoreMatch) {
+          const score = parseInt(scoreMatch[1])
+          console.log(score)
+        }
+      }
+    })
+  }
+}
+
+export async function evaluate(fen, depth) {
+  await startStockfish()
   stockfish.postMessage(`position fen ${fen}`)
   stockfish.postMessage(`go depth ${depth}`)
 }
