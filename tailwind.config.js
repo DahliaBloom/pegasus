a362a2c2e2afaf65690557061b134232f3e0a3ea
Benno <bennokolar@gmail.com>
2023-05-30 18:02:35 +0200

    Baboo!

--- tailwind.config.js
+++ tailwind.config.js
@@ -6,6 +6,29 @@ module.exports = {
   },
   plugins: [require('daisyui')],
   daisyui: {
-    themes: ['night']
+    themes: [
+      {
+        mytheme: {
+        
+"primary": "#3B82F6",
+        
+"secondary": "#F000B8",
+        
+"accent": "#7DD3FC",
+        
+"neutral": "#9ca3af",
+        
+"base-100": "#1f2937",
+        
+"info": "#447CC5",
+        
+"success": "#4ade80",
+        
+"warning": "#fbbf24",
+        
+"error": "#F82112",
+        },
+      },
+    ],
   }
 }
