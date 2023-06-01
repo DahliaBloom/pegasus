de35738cbca6cb9fb54a30216e43a9f259d373ed
Felix Steinbrecht <steinbrecht@proton.me>
2023-06-01 14:24:40 +0200

    Fix issue with EValBar not displaying right value

--- src/components/EvalBar.vue
+++ src/components/EvalBar.vue
@@ -1,69 +1,53 @@
 <script>
 export default {
-    props: {
-        evaluation: {
-            type: Number,
-            required: true
-        }
-    },
-    data() {
-        return {
-            height: 50.0,
-            heightString: "50%"
-        }
-    },
-    methods: {
-        update() {
-            console.log("update")
-            if (this.evaluation === 0.0) {
-                this.height = 50.0
-                this.heightString = this.height + "%"
-                return
-            }
-            let tmp = this.evaluation
-            if (tmp < 0.0) {
-                tmp = (-1) * tmp
-            }
-            if (tmp < 1.0) {
-                this.height = 50.0 + tmp * 10
-            }
-            else if (tmp < 3.0) {
-                this.height = 50.0 + tmp * 8
-            }
-            else if (tmp < 6.0) {
-                this.height = 50.0 + tmp * 6
-            }
-            else if (tmp < 9.0) {
-                this.height = 50.0 + tmp * 5
-            }
-            else {
-                this.height = 60.0
-            }
-            this.heightString = this.height + "%";
-            console.log(this.heightString)
-        }
-    },
-    beforeMount() {
-        this.update();
-    },
-    updated() {
-        this.update()
+  props: {
+    evaluation: {
+      type: Number,
+      required: true
     }
+  },
+  data() {
+    return {
+      height: 50.0,
+      heightString: '50%'
+    }
+  },
+  methods: {
+    update() {
+      console.log('update')
+      if (this.evaluation === 0.0) {
+        this.height = 50.0
+        this.heightString = this.height + '%'
+        return
+      }
+      this.height = 50 + 50 * (this.evaluation / 10)
+      this.heightString = this.height + '%'
+      console.log(this.heightString)
+    }
+  },
+  beforeMount() {
+    this.update()
+  },
+  updated() {
+    this.update()
+  }
 }
 </script>
 
 <template>
-    <div class=" bg-black w-12 h-full text-white text-center m-4 static font-bold">
-        <div v-if="this.evaluation < 0.0">{{ this.evaluation }}</div>
-        <div class=" bg-white w-12 text-center text-black m-4 absolute bottom-0 left-0 evalWhite">
-            <span class="absolute bottom-0 left-1/4" v-if="this.evaluation >= 0.0">{{ this.evaluation }}</span>
-        </div>
+  <div class="bg-black w-12 h-full text-white text-center m-4 static font-bold">
+    <div v-if="this.evaluation < 0">{{ this.evaluation * -1 }}</div>
+    <div class="bg-white w-12 text-center text-black m-4 absolute bottom-0 left-0 evalWhite">
+      <span class="absolute bottom-0 left-1/4" v-if="this.evaluation >= 0">{{
+        this.evaluation
+      }}</span>
     </div>
-    {{ this.heightString }}
+  </div>
+  {{ this.heightString }}
 </template>
 
 <style scoped>
 .evalWhite {
-    height: v-bind(heightString);
+  height: v-bind(heightString);
 }
 </style>
--- src/views/Analyze.vue
+++ src/views/Analyze.vue
@@ -1,16 +1,15 @@
 <script setup>
-import EvalBar from "../components/EvalBar.vue"
+import EvalBar from '../components/EvalBar.vue'
 </script>
 
 <template>
-  <div class="flex" style="height: 90vb;">
+  <div class="flex" style="height: 90vb">
     <EvalBar ref="evalBar" class="flex-none border-accent border-2" :evaluation="score"></EvalBar>
     <div>
       <input v-model="fen" type="text" placeholder="Enter FEN string" />
       <button @click="evaluatePosition">Evaluate</button>
       <div v-if="score !== 0">Evaluation score: {{ score }}</div>
       <p>r1b1k1nr/pppp1ppp/2n2q2/2b1p3/2B1P1Q1/2N5/PPPP1PPP/R1B1K1NR w KQkq - 6 5</p>
-      <p>Open console to see score!</p>
     </div>
   </div>
 </template>
@@ -29,9 +28,9 @@ export default {
   methods: {
     evaluatePosition() {
       evaluate(this.fen, (score) => {
-        console.log('Received score:', score);
+        console.log('Received score:', score)
         this.score = score
-      });
+      })
     }
   },
   components: {
