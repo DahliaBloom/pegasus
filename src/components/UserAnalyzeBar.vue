1741572ae3ff022e3bc7798bf6ec4ca586b39411
Benno <bennokolar@gmail.com>
2023-06-15 23:23:17 +0200

    fixed build error

--- src/components/UserAnalyzeBar.vue
+++ src/components/UserAnalyzeBar.vue
@@ -1,8 +1,8 @@
 <template>
     <div class="h-full w-full flex relative">
-        <img class="w-8 h-8 border-2 mr-2" src="src\assets\magnusCarlsen.png" alt="" v-if="color"
+        <img class="w-8 h-8 border-2 mr-2" src="/src/assets/magnusCarlsen.png" alt="" v-if="color"
             style="border-color: white;">
-        <img class="w-8 h-8 border-2 mr-2" src="src\assets\magnusCarlsen.png" alt="" v-else="color"
+        <img class="w-8 h-8 border-2 mr-2" src="/src/assets/magnusCarlsen.png" alt="" v-else="color"
             style="border-color: black;">
         <div class="flex flex-row items-baseline">
             <h2 class="text-xl font-bold">{{ username }}</h2>
