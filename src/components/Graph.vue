c05cc6e99627e1bc77ea2945e67a46599be985d3
idaks <gaming@idaks.de>
2023-07-04 12:41:59 +0200

    changed graph color

--- src/components/Graph.vue
+++ src/components/Graph.vue
@@ -5,28 +5,46 @@ export default{
     name: "Graph",
     props:{},
     mounted(){
-        const data = [65, 59, 80,55,10];
+        const data = [65, -59, 80,55,10];
         const ctx = document.getElementById("Graph");
         let label = [];
+        let x =[];
         let number = data.length;
         for(let i = 0;i<number;i++){
             label.push("");
+            x.push(0)
         } 
+
         const Graph = new Chart(ctx,{
             type: "line",
             data: {
                 labels:label,
                 datasets: [{ 
                     data: data ,
-                    fill: true,
-                    borderColor: 'rgb(0,0,0)',
-                }]},
+                    fill: {
+                        target: "origin",
+                        below: "rgb(0,0,0)",
+                        above: "rgb(255,255,255)"
+                    },
+                    borderWidth: 0,
+                    pointRadius: 0,
+                    tension: 0.4
+                }]
+            },
             options: {
                 plugins:{
                 legend: {
-                display: false,
+                    display: false,
                         },
-                    }}
+                    },
+                    scales: {
+                        y: {
+                            ticks:{
+                                color: "rgb(0,0,0)"
+                            }
+                        }
+                    }
+                }
         })
         Graph;
     }
@@ -36,7 +54,7 @@ export default{
 </script>
 
 <template>
-    <div class="chart-container" style="position: relative; height:30vh; width:50vw">
+    <div class="chart-container h-full w-full">
     <canvas id = "Graph"></canvas>
     </div>
 </template>
